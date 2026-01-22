10c0bf9bad4e8173a3caa99db484cc27bb7a163d
Haftamu Desta <haftamudesta>
2026-01-22 11:00:25 -0800

    Add types

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,13 @@
+import { getSession } from "@/lib/auth/auth";
+import connectDB from "@/lib/dbConnection";
+import { redirect } from "next/navigation";
 import React from "react";
 
-export default function page() {
+export default async function page() {
+  const session = await getSession();
+  if (!session?.user) {
+    redirect("/sign-in");
+  }
+  await connectDB();
   return <div>page</div>;
 }
