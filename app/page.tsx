8cacaf8c848b3a1f89b21c95c1f5c149e12db687
Haftamu Desta <haftamudesta>
2026-01-22 00:13:24 -0800

    Modify home

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         <section className="container mx-auto px-4 py-32">
           <div className="mx-auto max-w-4xl text-center">
             <h1 className="text-black mb-6 text-6xl font-bold">
-              A better way to track your hunt application.
+              A better way to hunt your Job application.
             </h1>
             <p className="text-muted-foreground mb-10 text-xl">
               Capture, organize, and manage your job search in one place.
